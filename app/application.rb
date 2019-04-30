8884480b76da4df4a1c978e02a657240cfa97f9e
David Acosta <davidacosta25081@yahoo.com>
2019-04-30 02:38:14 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,7 @@ class Application
       resp.write handle_search(search_term)
 
     elsif req.path.match(/cart/)
-      if @@cart.length < 1
+      if @@cart.empty?
         resp.write "Your cart is empty"
       else
         @@cart.each do |item|
