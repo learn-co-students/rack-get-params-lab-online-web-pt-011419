ec0a31be8f2bf70c5cfb4cdf6931a771ce5e929e
David Acosta <davidacosta25081@yahoo.com>
2019-04-30 02:24:22 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -11,17 +11,20 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
     elsif req.path.match(/cart/)
-      if @@cart.empty?
+      if @@cart.length < 1
         resp.write "Your cart is empty"
       else
-        @@cart.each do |cart_item|
-          resp.write "{cart_item}"
+        @@cart.each do |item|
+          resp.write "#{item}\n"
         end
       end
+
    elsif req.path.match(/add/)
      item = req.params["item"]
      if @@items.include?(item)
@@ -30,6 +33,7 @@ class Application
      else
        resp.write "We don't have that item"
      end
+
 
    else
       resp.write "Path Not Found"
@@ -45,3 +49,4 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+end
